d61f04c5843ef16719f2563b774eb9fe80a78857
diogoneto <dvnetoubzgmail.com>
2022-05-25 12:35:50 +0100

    Criada uma função para converter uma categoria e um livro em ContentValues

--- app/src/main/java/com/example/livros/Livro.kt
+++ app/src/main/java/com/example/livros/Livro.kt
@@ -1,5 +1,14 @@
 package com.example.livros
 
-data class Livro(var id: Long, var titulo: String, var autor: String, var iDCategoria: Long) {
+import android.content.ContentValues
 
+data class Livro(var id: Long, var titulo: String, var autor: String, var iDCategoria: Long) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
+        valores.put(TabelaBDlivros.CAMPO_TITULO, titulo)
+        valores.put(TabelaBDlivros.CAMPO_AUTOR, autor)
+        valores.put(TabelaBDlivros.CAMPO_CATEGORIA_ID, id)
+
+        return valores
+    }
 }
